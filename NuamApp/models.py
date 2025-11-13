4fb891bb091da69e34740ec2f3aa70037e498f2b
JackP390 <patatamati123@gmail.com>
2025-11-13 11:37:38 -0300

    Initial commit

---
+++ NuamApp/models.py
@@ -0,0 +1,93 @@
+from django.db import models
+from django.contrib.auth.hashers import make_password
+
+# Create your models here.
+class Cliente(models.Model):
+    id_cliente = models.CharField(max_length=15, primary_key=True, editable=False)
+    rut = models.CharField(max_length=12)
+    nombre = models.CharField(max_length=250)
+    direccion = models.CharField(max_length=150)
+    email = models.EmailField(max_length=100)
+    telefono = models.CharField(max_length=13)
+
+    def save(self, *args,**kwargs):
+        if not self.id_cliente:
+            last_client = Cliente.objects.all().order_by('id_cliente').last()
+
+            new_id_number = 1
+
+            if last_client:
+                last_id_str = last_client.id_cliente[1:]
+
+                try:
+                    last_id_number = int(last_id_str)
+                    new_id_number = last_id_number + 1
+                except ValueError:
+                    return ('Error con el valor ID')
+                
+            self.id_cliente = f'U{new_id_number}'
+
+        super().save(*args, **kwargs)
+
+    def __str__(self):
+        return f'{self.id_cliente} - {self.nombre}'
+    
+class Emisor(models.Model):
+    id_emisor = models.CharField(max_length=15, primary_key=True, editable=False)
+    rut = models.CharField(max_length=12)
+    nombre = models.CharField(max_length=100)
+
+    def save(self, *args, **kwargs):
+        if not self.id_emisor:
+            last_issuer = Emisor.objects.all().order_by('id_emisor').last()
+
+            new_id_number = 1
+
+            if last_issuer:
+                last_id_str = last_issuer.id_emisor[1:]
+
+                try:
+                    last_id_number = int(last_id_str)
+                    new_id_number = last_id_number + 1
+                except ValueError:
+                    return ('Error con el valor ID')
+                
+            self.id_emisor = f'E{new_id_number}'
+
+        super().save(*args, **kwargs)
+    
+    def __str__(self):
+        return f'{self.id_emisor} - {self.nombre}'
+
+class Corredor(models.Model):
+    id_corredor = models.CharField(max_length=15, primary_key=True, editable=False)
+    rut = models.CharField(max_length=12)
+    email = models.EmailField(max_length=100)
+    contraseña = models.CharField(max_length=128)
+
+    def save(self, *args, **kwargs):
+        #maneja la encriptación de la contraseña
+        if len(self.contraseña) < 128:
+            self.contraseña = make_password(self.contraseña)
+
+        if not self.id_corredor:
+            #ultimo corredor de bolsa
+            last_stockbroker = Corredor.objects.all().order_by('id_corredor').last()
+
+            new_id_number = 1
+
+            if last_stockbroker:
+                last_id_str = last_stockbroker.id_corredor[1:]
+
+                try:
+                    last_id_number = int(last_id_str)
+                    new_id_number = last_id_number + 1
+                except ValueError:
+                    return ('Error con el valor ID')
+                
+            self.id_corredor = f'E{new_id_number}'
+
+        super().save(*args, **kwargs)
+    
+    def __str__(self):
+        return f'{self.id_corredor} - {self.nombre}'
---
+++ NuamApp/views.py
@@ -0,0 +1,14 @@
+from django.shortcuts import render
+
+# Create your views here.
+def main(request):
+    return render(request, 'main.html')
+
+def login(request):
+    return render(request, 'login.html')
+
+def register(request):
+    return render(request, 'register.html')
+
+def holder(request):
+    return render(request, 'holder.html')
