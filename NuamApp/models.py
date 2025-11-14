bdad5e70da43913b4bc9fa31a14ae989cf0025cd
JackP390 <patatamati123@gmail.com>
2025-11-13 21:54:05 -0300

    se limpian los modelos para que sean más robustos

--- NuamApp/models.py
+++ NuamApp/models.py
@@ -1,176 +1,107 @@
 from django.db import models
-from django.contrib.auth.hashers import make_password
+from django.contrib.auth.models import AbstractBaseUser, BaseUserManager, PermissionsMixin
 from django.core.exceptions import ValidationError #para los errores de validacion
-import re #expresiones regulares (para depurar la contraseña)
+import re # Expresiones regulares, para depurar la contraseña
 
 SPECIAL_CHARS = r'[!@#$%^&*()_\-+=|\\/?\.,:;`~<>]'
 
 # Create your models here.
 class Cliente(models.Model):
-    id_cliente = models.CharField(max_length=15, primary_key=True, editable=False)
     rut = models.CharField(max_length=12)
     nombre = models.CharField(max_length=250)
     direccion = models.CharField(max_length=150)
     email = models.EmailField(max_length=100)
     telefono = models.CharField(max_length=13)
 
-    def save(self, *args,**kwargs):
-        if not self.id_cliente:
-            last_client = Cliente.objects.all().order_by('id_cliente').last()
-
-            new_id_number = 1
-
-            if last_client:
-                last_id_str = last_client.id_cliente[1:]
-
-                try:
-                    last_id_number = int(last_id_str)
-                    new_id_number = last_id_number + 1
-                except ValueError:
-                    return ('Error con el valor ID')
-                
-            self.id_cliente = f'U{new_id_number}'
-
-        super().save(*args, **kwargs)
-
     def __str__(self):
-        return f'{self.id_cliente} - {self.nombre}'
+        return f'{self.id} - {self.nombre}'
     
 class Emisor(models.Model):
-    id_emisor = models.CharField(max_length=15, primary_key=True, editable=False)
     rut = models.CharField(max_length=12)
     nombre = models.CharField(max_length=100)
-
-    def save(self, *args, **kwargs):
-        if not self.id_emisor:
-            last_issuer = Emisor.objects.all().order_by('id_emisor').last()
-
-            new_id_number = 1
-
-            if last_issuer:
-                last_id_str = last_issuer.id_emisor[1:]
-
-                try:
-                    last_id_number = int(last_id_str)
-                    new_id_number = last_id_number + 1
-                except ValueError:
-                    return ('Error con el valor ID')
-                
-            self.id_emisor = f'E{new_id_number}'
-
-        super().save(*args, **kwargs)
+    estado = models.BooleanField(default=True)
     
     def __str__(self):
-        return f'{self.id_emisor} - {self.nombre}'
+        return f'{self.id} - {self.nombre}'
 
-class Corredor(models.Model):
-    id_corredor = models.CharField(max_length=15, primary_key=True, editable=False)
+# Esto maneja el hashing de manera automatica
+class CustomCorredorManager(BaseUserManager):
+    def create_user(self, email, password=None, **extra_fields):
+        # Esto se asegura que el email sea requerido y se normalice (por ejemplo que se ponga en minúsculas)
+        if not email:
+            raise ValueError('El email debe ser configurado')
+        email = self.normalize_email(email)
+
+        # Crea y guarda el corredor
+        user = self.model(email=email, **extra_fields)
+        user.set_password(password) # esto es para hashear la contraseña antes de guardarla
+        user.save(using=self._db)
+        return user
+    
+    def create_superuser(self, email, password=None, **extra_fields):
+        extra_fields.setdefault('is_staff', True) # Maneja datos importantes del codigo de django
+        extra_fields.setdefault('is_superuser', True) # Se puede hacer lo que sea con esté campo (por eso no lo tiene nadie)
+        extra_fields.setdefault('is_active', True) # reemplaza el campo estado
+
+        if extra_fields.get('is_staff') is not True:
+            raise ValueError('Superuser debe tener is_staff=True.')
+        if extra_fields.get('is_superuser') is not True:
+            raise ValueError('Superuser debe tener is_superuser=True.')
+        
+        return self.create_user(email, password, **extra_fields)
+
+class Corredor(AbstractBaseUser, PermissionsMixin):
+    USERNAME_FIELD = 'email' #el campo que se usa en el login
+    REQUIRED_FIELDS = ['nombre', 'rut', 'rol'] #campos que se piden al crear usuario
+
     nombre = models.CharField(max_length=100)
-    rut = models.CharField(max_length=12)
-    email = models.EmailField(max_length=100)
-    contraseña = models.CharField(max_length=128)
+    rut = models.CharField(max_length=12, unique=True)
+    email = models.EmailField(max_length=100, unique=True)
     rol = models.CharField(max_length=50)
-    estado = models.BooleanField(default=False)
+    is_active = models.BooleanField(default=True) # is_active es el campo estado, el AbstractBaseUser lo pide obligatoriamente con ese nombre
+    is_staff = models.BooleanField(default=False)
+
+    objects = CustomCorredorManager()
 
     def clean(self): #para realizar la validacion
         super().clean() #llama al metodo clean padre/raiz/principal
 
-        password = self.contraseña
+        password = self.password
         if len(password) < 128: #con esto solo se valida si es texto no si es un cifrado
             if len(password) < 8: #La contraseña debe tener minimo 8 caracteres como dice abajo
-                raise ValidationError({'contraseña': "La contraseña debe tener al menos 8 caracteres."})
+                raise ValidationError({'password': "La contraseña debe tener al menos 8 caracteres."})
                 #se llama al validation error con el nombre del campo para que se muestre el error más claro
 
             if not re.search(r'[A-Z]', password): #La contraseña debe tener una letra mayuscula... como dice abajo
-                raise ValidationError({'contraseña': "La contraseña debe contener al menos una letra mayúscula."})
+                raise ValidationError({'password': "La contraseña debe contener al menos una letra mayúscula."})
             
             if not re.search(SPECIAL_CHARS, password): #un caracter especial, como dice abajo
-                raise ValidationError({'contraseña': f"La contraseña debe contener al menos un caracter especial: {SPECIAL_CHARS}"})
-
-
-    def save(self, *args, **kwargs):
-        #maneja la encriptación de la contraseña
-        if len(self.contraseña) < 128:
-            self.contraseña = make_password(self.contraseña)
-
-        if not self.id_corredor:
-            #ultimo corredor de bolsa
-            last_stockbroker = Corredor.objects.all().order_by('id_corredor').last()
-
-            new_id_number = 1
-
-            if last_stockbroker:
-                last_id_str = last_stockbroker.id_corredor[1:]
-
-                try:
-                    last_id_number = int(last_id_str)
-                    new_id_number = last_id_number + 1
-                except ValueError:
-                    return ('Error con el valor ID')
-                
-            self.id_corredor = f'CO{new_id_number}'
-
-        super().save(*args, **kwargs)
+                raise ValidationError({'password': f"La contraseña debe contener al menos un caracter especial: {SPECIAL_CHARS}"})
     
     def __str__(self):
-        return f'{self.id_corredor} - {self.nombre}'
+        return f'{self.email}'
     
 #calificación tributaria
 class Calificacion(models.Model):
-    id_calificacion = models.CharField(max_length=15, primary_key=True, editable=False)
+    id_cliente = models.ForeignKey('Cliente', on_delete=models.PROTECT) #si el cliente es eliminado la calificacion no se elimina
+    id_emisor = models.ForeignKey('Emisor', on_delete=models.PROTECT) #lo mismo con el emisor 
     año_tributario = models.IntegerField(null=True)
     fecha_creacion_registro = models.DateTimeField(auto_now_add=True)
     ultima_modificacion = models.DateTimeField(auto_now=True)
     #campos que estan en el E-R: [ID_Cliente, ID_Emisor]
-
-    def save(self, *args, **kwargs):
-        if not self.id_calificacion:
-            last_qualification = Calificacion.objects.all().order_by('id_calificacion').last()
-
-            new_id_number = 1
-
-            if last_qualification:
-                last_id_str = last_qualification.id_calificacion[1:]
-
-                try:
-                    last_id_number = int(last_id_str)
-                    new_id_number = last_id_number + 1
-                except ValueError:
-                    return ('Error con el valor ID')
-                
-            self.id_calificacion = f'C{new_id_number}'
-
-        super().save(*args, **kwargs)
     
     def __str__(self):
-        return f'{self.id_calificacion} - {self.nombre}'
+        return f'{self.id} - {self.año_tributario}'
     
 #detalle calificacion
 class Detalle_c(models.Model):
-    id_detalle = models.CharField(max_length=15, primary_key=True, editable=False)
+    id_calificacion = models.ForeignKey('Calificacion', on_delete=models.CASCADE) #si la calificación es eliminada se elimina su detalle
+    id_corredor = models.ForeignKey('Corredor', on_delete=models.PROTECT) #a diferencia de si se "despide" el corredor
     tipo_dato = models.CharField(max_length=10)
-    valor_monto = models.FloatField(null=True)
-    valor_factor = models.FloatField(null=True)
+    valor_monto = models.DecimalField(max_digits=15, decimal_places=2, null=True)
+    valor_factor = models.DecimalField(max_digits=15, decimal_places=2, null=True)
+    fecha_creacion_detalle = models.DateTimeField(auto_now_add=True)
     # campos que estan en el E-R: [ID_Calificacion, ID_Corredor, Emisor, fecha_creacion_registro]
     
-    def save(self, *args, **kwargs):
-        if not self.id_detalle:
-            last_detail = Detalle_c.objects.all().order_by('id_detalle').last()
-
-            new_id_number = 1
-
-            if last_detail:
-                last_id_str = last_detail.id_detalle[1:]
-
-                try:
-                    last_id_number = int(last_id_str)
-                    new_id_number = last_id_number + 1
-                except ValueError:
-                    return ('Error con el valor ID')
-                
-            self.id_detalle = f'DC{new_id_number}'
-
-        super().save(*args, **kwargs)
-    
     def __str__(self):
-        return f'{self.id_detalle} - {self.nombre}'
+        return f'{self.id} - {self.tipo_dato}'
