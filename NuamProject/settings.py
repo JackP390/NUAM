046bb50af0fba9599dc6450abcbfeabc0ebfa913
JackP390 <patatamati123@gmail.com>
2025-12-07 15:28:46 -0300

    continuación del commit anterior

--- NuamProject/settings.py
+++ NuamProject/settings.py
@@ -123,3 +123,6 @@ STATIC_URL = 'static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 AUTH_USER_MODEL = 'NuamApp.Corredor'
+
+LOGIN_REDIRECT_URL = 'redireccion_login'
+LOGOUT_REDIRECT_URL = 'login'
